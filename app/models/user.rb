8f5aa5f53a26f1cb21e82112ae580fcb7b627836
Kade Kobussen <kadekobussen50@gmail.com>
2023-05-08 20:54:34 -0700

    start building tests for user. change user to take in first and last name instead of just name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ApplicationRecord
-  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
   has_secure_password
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe user, type: :model do
+  describe 'Validations' do
+   before do
+     @user = @user.build(
+       email: 'Test.Mctest@test.com',
+       password: 'pass',
+       password_confirmation: 'pass'
+       first_name:'test',
+       last_name: 'McTest'        
+     )
+   end
+
+   it 'password is at least 8 chars' do
+
+   end
+
+   it '' do
+
+   end
+
+   it '' do
+
+   end
+
+   it '' do
+
+   end
+
+   it '' do
+
+   end
+
+   it '' do
+
+   end
+
+   it '' do
+
+   end
+
+
+
 end
