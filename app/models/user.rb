414c1024372bc15a3deb1999b050c40e74226ed0
Kade Kobussen <kadekobussen50@gmail.com>
2023-05-08 21:54:00 -0700

    add requrements to user .rb add tests for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 8 }, presence: true
   has_secure_password
 
   def self.authenticate_with_credentials(email, password)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,45 +1,76 @@
 require 'rails_helper'
 
-RSpec.describe user, type: :model do
+RSpec.describe User, type: :model do
+
   describe 'Validations' do
-   before do
-     @user = @user.build(
-       email: 'Test.Mctest@test.com',
-       password: 'pass',
-       password_confirmation: 'pass'
-       first_name:'test',
-       last_name: 'McTest'        
-     )
-   end
+    before do
+      @user = User.new(
+        email: 'Test.Mctest@test.com',
+        password: '123456789',
+        password_confirmation: '123456789',
+        first_name:'test',
+        last_name: 'McTest'        
+      )
+    end
 
-   it 'password is at least 8 chars' do
+    it 'is valid with valid attributes' do
+      expect(@user).to be_valid
+    end
 
-   end
+    it 'is not valid with no email' do
+      @user.email = nil
+      expect(@user).to_not be_valid
+    end
 
-   it '' do
+    it 'is not valid with no password' do
+      @user.password = nil
+      expect(@user).to_not be_valid
+    end
 
-   end
+    it 'is not valid with a password confirmation that does not match the password' do
+      @user.password_confirmation = '1123456789'
+      expect(@user).to_not be_valid
+    end
 
-   it '' do
+    it 'is not valid with no first name' do
+      @user.first_name = nil
+      expect(@user).to_not be_valid
+    end
 
-   end
+    it 'is not valid with no last name' do
+      @user.last_name = nil
+      expect(@user).to_not be_valid
+    end
 
-   it '' do
+    it 'is not valid with a duplicate email (case-insensitive)' do
+      @user.save
+      new_user = User.new(
+        email: 'Test.Mctest@test.com',
+        password: '123456789',
+        password_confirmation: '123456789',
+        first_name:'test',
+        last_name: 'McTest'        
+      )
+      expect(new_user).to_not be_valid
+    end
 
-   end
+    it 'is not valid with a password shorter than 8 characters' do
+      @user.password = '1234567'
+      @user.password_confirmation = '1234567'
+      expect(@user).to_not be_valid
+    end
+  end
 
-   it '' do
-
-   end
-
-   it '' do
-
-   end
-
-   it '' do
-
-   end
-
-
-
+  describe '.authenticate_with_credentials' do
+    before do
+      @user = User.new(
+        email: 'test@test.com',
+        password: 'password',
+        password_confirmation: 'password',
+        first_name: 'John',
+        last_name: 'Doe'
+      )
+      @user.save
+    end
+  end
 end
